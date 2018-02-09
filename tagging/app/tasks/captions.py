3b83c3b883ba0adf34d6c62ec941bfb935631342
Shailesh <kochhar@users.noreply.github.com>
2018-02-09 17:35:58 +0530

    Added tasks for processing.

--- tagging/app/tasks/captions.py
+++ tagging/app/tasks/captions.py
@@ -3,14 +3,70 @@ captions.py
 
 module containing tasks for captions
 """
-import urllib
+import html
+import logging
+import os
+from os import path
+import requests
+import subprocess
+import tempfile
+from xml.etree import ElementTree as ET
 
-from app import celery, db
+from app import app, celery, db
+
+CAPTION_SERVICE_URL = 'http://video.google.com/timedtext'
+HEIDELTIME_WD = path.join(app.root_path, app.config['HEIDELTIME_LIB_DIR'])
+HEIDELTIME_CMD_ARGS = \
+    'java -jar de.unihd.dbs.heideltime.standalone.jar -t narratives'.split()
+RESPONSE_SERIAL_FIELDS = [
+#    'content',
+#    'elapsed',
+    'encoding',
+    'headers',
+    'links',
+    'ok',
+    'reason',
+    'status_code',
+    'text',
+    'url'
+]
 
 
 @celery.task
 def youtube_captions_from_video(video_id):
     """Given a youtube video id, fetches the captions for the video."""
-    CAPTION_SERVICE_URL = 'http://video.google.com/timedtext'
+    caption_params = {'lang': 'en', 'v': video_id}
+    resp = requests.get(CAPTION_SERVICE_URL, params=caption_params)
+    if resp.headers.get('Content-Type') == 'application/json':
+        result = {'json': resp.json()}
+    else:
+        result = {}
 
-    ?lang=en&v=JFpanWNgfQY
+    result.update(dict((f, getattr(resp, f)) for f in RESPONSE_SERIAL_FIELDS))
+    # headers is an instance of CaseInsensitiveDict, convert to plain dic
+    if 'headers' in result:
+        result['headers'] = dict(result['headers'])
+    return result
+
+
+@celery.task
+def events_from_captions(caption_result, video_id):
+    """Given captions as string and a video_id, extracts events."""
+    fd, filename = tempfile.mkstemp(suffix='.txt', prefix='cap-',
+                                    dir=app.config['HEIDELTIME_TMPINPUT_DIR'])
+
+    transcript_xml = caption_result['text']
+    transcript_root = ET.fromstring(transcript_xml)
+
+    f = os.fdopen(fd, 'w')
+    for i, text_node in enumerate(transcript_root.findall('text')):
+        unescaped = html.unescape(text_node.text)
+        f.write(unescaped)
+        f.write(' ')
+    f.close()
+    logging.debug('Wrote {} caption chunks for extraction'.format(i+1))
+
+    cmd_args = HEIDELTIME_CMD_ARGS + [filename]
+    logging.info('Invoking HeidelTime with {}'.format(' '.join(cmd_args)))
+    res = subprocess.run(cmd_args, cwd=HEIDELTIME_WD, stdout=subprocess.PIPE)
+    return res.stdout.decode('utf-8')
